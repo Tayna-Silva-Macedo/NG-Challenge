22b10bab14c9912b38a97b440e3cb75ca268de1a
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-11-15 10:16:08 -0300

    chore: configurando o TypeScript

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import { StatusCodes } from 'http-status-codes';
+import errorMiddleware from './middlewares/error';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.status(StatusCodes.OK).send('Express + TypeScript');
 });
+
+app.use(errorMiddleware);
 
 export default app;
