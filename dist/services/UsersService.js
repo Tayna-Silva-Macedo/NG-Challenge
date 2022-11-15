cb9ee553f54229c708f046ba080504e75af5315d
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-11-15 15:49:34 -0300

    feat: não permite criar usuário que já existe ou com menos de 3 caracteres

--- dist/services/UsersService.js
+++ dist/services/UsersService.js
@@ -8,16 +8,38 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const http_status_codes_1 = require("http-status-codes");
+const HttpException_1 = __importDefault(require("../helpers/HttpException"));
 class UsersService {
     constructor(usersModel, accountsService) {
         this.usersModel = usersModel;
         this.accountsService = accountsService;
     }
+    static validUsername(username) {
+        if (username.length < 3)
+            throw new HttpException_1.default(http_status_codes_1.StatusCodes.BAD_REQUEST, 'username must be at least 3 characters');
+    }
+    usernameExists(username) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const user = yield this.usersModel.findOne({ where: { username } });
+            if (user)
+                throw new HttpException_1.default(http_status_codes_1.StatusCodes.CONFLICT, 'user already registered');
+        });
+    }
     create(obj) {
         return __awaiter(this, void 0, void 0, function* () {
+            UsersService.validUsername(obj.username);
+            yield this.usernameExists(obj.username);
             const accountId = yield this.accountsService.create();
-            const newUser = yield this.usersModel.create({ username: obj.username, password: obj.password, accountId });
+            const newUser = yield this.usersModel.create({
+                username: obj.username,
+                password: obj.password,
+                accountId,
+            });
             return newUser;
         });
     }
--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,16 +1,43 @@
-import User from "../database/models/User";
-import IUser from "../interfaces/IUser";
-import IUsersService from "../interfaces/services/IUsersService";
-import AccountsService from "./AccountsService";
+import { StatusCodes } from 'http-status-codes';
+import User from '../database/models/User';
+import HttpException from '../helpers/HttpException';
+import IUser from '../interfaces/IUser';
+import IUsersService from '../interfaces/services/IUsersService';
+import AccountsService from './AccountsService';
 
 export default class UsersService implements IUsersService {
-  constructor(private usersModel: typeof User, private accountsService: AccountsService) {}
+  constructor(
+    private usersModel: typeof User,
+    private accountsService: AccountsService
+  ) {}
 
-  async create(obj: IUser): Promise<IUser> {
+  private static validUsername(username: string): void {
+    if (username.length < 3)
+      throw new HttpException(
+        StatusCodes.BAD_REQUEST,
+        'username must be at least 3 characters'
+      );
+  }
+
+  private async usernameExists(username: string): Promise<void> {
+    const user = await this.usersModel.findOne({ where: { username } });    
+
+    if (user)
+      throw new HttpException(StatusCodes.CONFLICT, 'user already registered');
+  }
+
+  async create(obj: IUser): Promise<IUser> {    
+    UsersService.validUsername(obj.username);
+    await this.usernameExists(obj.username); 
+
     const accountId = await this.accountsService.create();
 
-    const newUser = await this.usersModel.create({username: obj.username, password: obj.password, accountId});
-    
+    const newUser = await this.usersModel.create({
+      username: obj.username,
+      password: obj.password,
+      accountId,
+    });
+
     return newUser;
   }
 }
