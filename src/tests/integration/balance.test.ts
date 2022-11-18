f4a53a70b786f8d079ccd0a338a09d0a39dde8c2
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-11-18 11:05:02 -0300

    test: verifica se não é retornado o saldo quando não é passado um token ou ele é inválido

--- src/tests/integration/balance.test.ts
+++ src/tests/integration/balance.test.ts
@@ -15,6 +15,51 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Testes da rota /balance', () => {
+  describe('Verifica se não é retornado o saldo quando o token não é fornecido', () => {
+    let responseBalance: Response;
+
+    before(async () => {
+      responseBalance = await chai
+        .request(app)
+        .get('/balance')
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('retorna status 401', () => {
+      expect(responseBalance.status).to.be.equal(401);
+    });
+
+    it('retorna mensagem de erro', () => {
+      expect(responseBalance.body).to.be.deep.equal({ message: 'Token not found' });
+    });
+  });
+
+  describe('Verifica se não é retornado o saldo quando o token é inválido', () => {
+    let responseBalance: Response;
+
+    before(async () => {
+      responseBalance = await chai
+        .request(app)
+        .get('/balance')
+        .set('authorization', 'invalid_token');
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('retorna status 401', () => {
+      expect(responseBalance.status).to.be.equal(401);
+    });
+
+    it('retorna mensagem de erro', () => {
+      expect(responseBalance.body).to.be.deep.equal({ message: 'Token must be a valid token' });
+    });
+  });
+
   describe('Verifica se é retornado o saldo na conta corretamente', () => {
     let responseRegister: Response;
     let responseLogin: Response;
