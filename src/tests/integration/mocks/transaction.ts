e68aa1bdf729a3eee7a995bc7ca1f8cf424c7858
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-11-19 11:46:07 -0300

    test: verifica que é possível listar todas as transferências e filtrar pela data

--- src/tests/integration/mocks/transaction.ts
+++ src/tests/integration/mocks/transaction.ts
@@ -6,4 +6,73 @@ const transactionCreated = {
   createdAt: '2022-11-19T14:19:39.448Z' as unknown as Date,
 };
 
-export { transactionCreated };
+const allTransactionsOutput = [
+  {
+    id: 1,
+    debitedAccountId: 1,
+    creditedAccountId: 3,
+    value: 50,
+    createdAt: '2022-11-18T22:17:12.687Z' as unknown as Date,
+  },
+  {
+    id: 2,
+    debitedAccountId: 1,
+    creditedAccountId: 4,
+    value: 10,
+    createdAt: '2022-11-19T22:17:38.345Z' as unknown as Date,
+  },
+  {
+    id: 3,
+    debitedAccountId: 1,
+    creditedAccountId: 5,
+    value: 3,
+    createdAt: '2022-11-18T22:17:49.922Z' as unknown as Date,
+  },
+  {
+    id: 4,
+    debitedAccountId: 5,
+    creditedAccountId: 1,
+    value: 75,
+    createdAt: '2022-11-19T22:18:23.591Z' as unknown as Date,
+  },
+  {
+    id: 8,
+    debitedAccountId: 3,
+    creditedAccountId: 1,
+    value: 50,
+    createdAt: '2022-11-18T22:19:59.805Z' as unknown as Date,
+  },
+  {
+    id: 9,
+    debitedAccountId: 4,
+    creditedAccountId: 1,
+    value: 200,
+    createdAt: '2022-11-20T22:21:08.900Z' as unknown as Date,
+  },
+];
+
+const allTransactionsFilterOutput = [
+  {
+    id: 1,
+    debitedAccountId: 1,
+    creditedAccountId: 3,
+    value: 50,
+    createdAt: '2022-11-18T22:17:12.687Z' as unknown as Date,
+  },
+  {
+    id: 3,
+    debitedAccountId: 1,
+    creditedAccountId: 5,
+    value: 3,
+    createdAt: '2022-11-18T22:17:49.922Z' as unknown as Date,
+  },
+  {
+    id: 8,
+    debitedAccountId: 3,
+    creditedAccountId: 1,
+    value: 50,
+    createdAt: '2022-11-18T22:19:59.805Z' as unknown as Date,
+  },
+];
+
+export { transactionCreated, allTransactionsOutput, allTransactionsFilterOutput };
--- src/tests/integration/transactions.test.ts
+++ src/tests/integration/transactions.test.ts
@@ -7,7 +7,7 @@ import User from '../../database/models/User';
 
 import { accountOutput, accountOutput2 } from './mocks/account';
 import { userCreated, userCreated2, userFind, userFind2 } from './mocks/user';
-import { transactionCreated } from './mocks/transaction'
+import { allTransactionsFilterOutput, allTransactionsOutput, transactionCreated } from './mocks/transaction'
 
 import { Response } from 'superagent';
 import app from '../../app';
@@ -207,6 +207,102 @@ describe('Testes da rota /transactions', () => {
 
     it('retorna a transferência criada', () => {
       expect(responseTransaction.body).to.be.deep.equal(transactionCreated);
+    });
+  });
+
+  describe('Verifica se é possível listar todas as transferências com sucesso', () => {
+    let responseLogin: Response;
+    let responseTransaction: Response;
+
+    before(async () => {
+      sinon
+        .stub(User, 'create').resolves(userCreated as User)
+      sinon
+        .stub(User, 'findOne')
+        .onCall(0)
+        .resolves(null)
+        .onCall(1)
+        .resolves(userFind as User)
+      sinon
+        .stub(Account, 'create').resolves(accountOutput as Account)
+      sinon.stub(Transaction, 'findAll').resolves(allTransactionsOutput as Transaction[]);
+
+      await chai.request(app).post('/register').send({
+        username: 'taynasm',
+        password: '1234567AbC',
+      });
+
+      responseLogin = await chai.request(app).post('/login').send({
+        username: 'taynasm',
+        password: '1234567AbC',
+      });
+
+      const token = responseLogin.body.token;
+
+      responseTransaction = await chai
+        .request(app)
+        .get('/transactions')
+        .set('authorization', token);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('retorna status 200', () => {
+      expect(responseTransaction.status).to.be.equal(200);
+    });
+
+    it('retorna a transferência criada', () => {
+      expect(responseTransaction.body).to.be.deep.equal(allTransactionsOutput);
+    });
+  });
+
+  describe('Verifica se é possível listar todas as transferências filtrando pela data', () => {
+    let responseLogin: Response;
+    let responseTransaction: Response;
+
+    before(async () => {
+      sinon
+        .stub(User, 'create').resolves(userCreated as User)
+      sinon
+        .stub(User, 'findOne')
+        .onCall(0)
+        .resolves(null)
+        .onCall(1)
+        .resolves(userFind as User)
+      sinon
+        .stub(Account, 'create').resolves(accountOutput as Account)
+      sinon.stub(Transaction, 'findAll').resolves(allTransactionsFilterOutput as Transaction[]);
+
+      await chai.request(app).post('/register').send({
+        username: 'taynasm',
+        password: '1234567AbC',
+      });
+
+      responseLogin = await chai.request(app).post('/login').send({
+        username: 'taynasm',
+        password: '1234567AbC',
+      });
+
+      const token = responseLogin.body.token;
+
+      responseTransaction = await chai
+        .request(app)
+        .get('/transactions?2022-11-18')
+        .set('authorization', token);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('retorna status 200', () => {
+      expect(responseTransaction.status).to.be.equal(200);
+    });
+
+    it('retorna a transferência criada', () => {
+      expect(responseTransaction.body).to.be.deep.equal(allTransactionsFilterOutput);
     });
   });
 });
