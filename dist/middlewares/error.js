0b005397191462c914d09b8cb83a30d762deb7a6
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-11-20 09:39:13 -0300

    fix: adiciona pasta dist ao .gitignore

--- dist/middlewares/error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorMiddleware = (err, _req, res, _next) => {
-    const { status, message } = err;
-    res.status(status || 500).json({ message });
-};
-exports.default = errorMiddleware;
